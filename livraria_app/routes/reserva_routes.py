5cbb43c9c6d4e1c876816bc19c404c6993d0b138
EnzoSenatori <enzosenatori2@gmail.com>
2026-04-21 22:38:32 -0300

    ES-feat: reserva_routes

---
+++ livraria_app/routes/reserva_routes.py
@@ -0,0 +1,14 @@
+# Camada de Rotas - Endpoints de Reservas
+# Responsabilidade: mapear URL/método HTTP para funções do Controller.
+
+from flask import Blueprint, request, jsonify
+
+from livraria_app.controllers.reserva_controller import reservar
+
+reserva_routes = Blueprint("reserva_routes", __name__) # BluePrint é ideal para caso o projeto cresça
+
+@reserva_routes.post("/api/reservas")
+def rota_reservar(): # deixei "rota_ [...]" para facilitar depuração em caso de erros --> deixa explícito que a função só existe na camada de rotas
+    dados = request.get_json(silent=True) or {}
+    resposta, status = reservar(dados)
+    return jsonify(resposta), status
